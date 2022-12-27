b11580eeed4aee9b8c047cfe97e67e30a50dcfe1
David Kyalo <davidmkyalo@gmail.com>
2022-12-27 20:38:41 +0300

    switching to zana

--- src/zana/django/apps.py
+++ src/zana/django/apps.py
@@ -4,11 +4,12 @@ from django.db.models.signals import class_prepared
 
 @class_prepared.connect
 def __on_class_prepared(sender, **kwds):
-    from .models.aliases import ImplementsAliases, _patch_model
+    from zana.django.models.aliases import ImplementsAliases, _patch_model
     issubclass(sender, ImplementsAliases) and _patch_model(sender)
     
 
 
-class Djx(AppConfig):
+class ZanaAppConfig(AppConfig):
     name = f'{__package__}'
+    verbose_name = 'Zana'
 
